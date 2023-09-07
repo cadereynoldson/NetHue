40fd237ff141d9c3017e8572d77cf3638a5a9c04
Cade Reynoldson <cader>
2023-09-06 18:40:36 -0700

    forgot to save a file

--- src/Models/Hue/Scene/HueScene.cs
+++ src/Models/Hue/Scene/HueScene.cs
@@ -11,4 +11,15 @@ public record HueScene : HueResource
     /// The group associated with this scene. 
     /// </summary>
     public HueResourceIdentifier Group { get; set; } = default!;
+
+    /// <summary>
+    /// The speed of the dynamic palette for this scene. 
+    /// </summary>
+    public double Speed { get; set; } = default!;
+
+    /// <summary>
+    /// Indicates where to automatically start the dynamically on active recall.
+    /// </summary>
+    public bool AutoDynamic { get; set; } = default!;
+    public string Status { get; set; } = default!;
 }
--- src/Models/Hue/Scene/HueSceneAction.cs
+++ src/Models/Hue/Scene/HueSceneAction.cs
@@ -1,20 +1,48 @@
 namespace NetHue;
 
+/// <summary>
+/// Represents an action to be executed.
+/// </summary>
 public record HueSceneAction
 {
+    /// <summary>
+    /// The identifier of the light to execute the action on
+    /// </summary>
     public HueResourceIdentifier Target { get; set; } = default!;
+
+    /// <summary>
+    /// Indicates if the resource should be turned on or off.
+    /// </summary>
     public bool? On { get; set; }
 
+    /// <summary>
+    /// Indicates the brightness to apply to the resource. 
+    /// </summary>
     public double? Brightness { get; set; }
 
+    /// <summary>
+    /// Indicates the CieColor to apply to the resource. 
+    /// </summary>
     public CieColor? Color { get; set; }
 
+    /// <summary>
+    /// Indicates the ColorTemperature to apply to the resource to. 
+    /// </summary>
     public MiredColor? ColorTemperature { get; set; }
 
+    /// <summary>
+    /// Indicates the gradient to apply to the resource. 
+    /// </summary>
     public HueGradient? Gradient { get; set; }
 
+    /// <summary>
+    /// Indicates the effect to apply to the resource. 
+    /// </summary>
     public string? Effect { get; set; }
 
+    /// <summary>
+    /// The duration of a light transition. 
+    /// </summary>
     public int? DynamicDuration { get; set; }
     
 }
